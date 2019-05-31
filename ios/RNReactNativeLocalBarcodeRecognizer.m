05c7b04f1e0af0153f0659f25f679567047a81f7
Ezequiel Zuccon <zombierabbit@users.noreply.github.com>
2019-05-31 16:34:13 -0300

    Update RNReactNativeLocalBarcodeRecognizer.m
    Mod for take just a peace of the image

--- ios/RNReactNativeLocalBarcodeRecognizer.m
+++ ios/RNReactNativeLocalBarcodeRecognizer.m
@@ -1,6 +1,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <CoreImage/CoreImage.h>
 #import <AVFoundation/AVFoundation.h>
 #import "RNReactNativeLocalBarcodeRecognizer.h"
 #import <ZXingObjC/ZXingObjC.h>
@@ -16,7 +17,7 @@
 
 + (NSDictionary *)validCodeTypes
 {
-    return @{
+    /*return @{
              @"upc_e" : AVMetadataObjectTypeUPCECode,
              @"code39" : AVMetadataObjectTypeCode39Code,
              @"code39mod43" : AVMetadataObjectTypeCode39Mod43Code,
@@ -30,7 +31,9 @@
              @"interleaved2of5" : AVMetadataObjectTypeInterleaved2of5Code,
              @"itf14" : AVMetadataObjectTypeITF14Code,
              @"datamatrix" : AVMetadataObjectTypeDataMatrixCode
-             };
+             };*/
+    
+    return @{ @"pdf417" : AVMetadataObjectTypePDF417Code };
 }
 
 RCT_EXPORT_MODULE(LocalBarcodeRecognizer);
@@ -47,9 +50,12 @@ RCT_EXPORT_METHOD(decode:(NSString *)base64EncodedImage
     dispatch_async(queue, ^{
         UIImage* image =[self decodeBase64ToImage:base64EncodedImage];
         
+        // First cycle
         for (int i = 0; i < 4; i++)
         {
-            ZXResult* result = [self decodeBarCode:image];
+            UIImage* cuttedImage = [self cutImage:image];
+            
+            ZXResult* result = [self decodeBarCode:cuttedImage];
             
             if(result){
                 resolve(result.text);
@@ -95,6 +101,131 @@ RCT_EXPORT_METHOD(decode:(NSString *)base64EncodedImage
     
     return newImage;
 }
+    
+- (UIImage*)cutImage:(UIImage*)sourceImage
+{
+    CGFloat imgWidth = sourceImage.size.width;
+    CGFloat imgheight = sourceImage.size.height;
+    CGRect rightImgFrame = CGRectMake(imgWidth * 0.5, imgheight * 0.75, imgWidth, imgheight);
+    
+    CGImageRef bottomRight = CGImageCreateWithImageInRect(sourceImage.CGImage, rightImgFrame);
+    
+    UIImage* rightImage = [UIImage imageWithCGImage:bottomRight];
 
+    CGImageRelease(bottomRight);
+    
+    return rightImage;
+}
+    
+- (UIImage *)negativeImage:(UIImage *)image
+{
+    // get width and height as integers, since we'll be using them as
+    // array subscripts, etc, and this'll save a whole lot of casting
+    CGSize size = image.size;
+    int width = size.width;
+    int height = size.height;
+    
+    // Create a suitable RGB+alpha bitmap context in BGRA colour space
+    CGColorSpaceRef colourSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *memoryPool = (unsigned char *)calloc(width*height*4, 1);
+    CGContextRef context = CGBitmapContextCreate(memoryPool, width, height, 8, width * 4, colourSpace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast);
+    CGColorSpaceRelease(colourSpace);
+    
+    // draw the current image to the newly created context
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), [image CGImage]);
+    
+    // run through every pixel, a scan line at a time...
+    for(int y = 0; y < height; y++)
+    {
+        // get a pointer to the start of this scan line
+        unsigned char *linePointer = &memoryPool[y * width * 4];
+        
+        // step through the pixels one by one...
+        for(int x = 0; x < width; x++)
+        {
+            // get RGB values. We're dealing with premultiplied alpha
+            // here, so we need to divide by the alpha channel (if it
+            // isn't zero, of course) to get uninflected RGB. We
+            // multiply by 255 to keep precision while still using
+            // integers
+            int r, g, b;
+            if(linePointer[3])
+            {
+                r = linePointer[0] * 255 / linePointer[3];
+                g = linePointer[1] * 255 / linePointer[3];
+                b = linePointer[2] * 255 / linePointer[3];
+            }
+            else
+            r = g = b = 0;
+            
+            // perform the colour inversion
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+            
+            // multiply by alpha again, divide by 255 to undo the
+            // scaling before, store the new values and advance
+            // the pointer we're reading pixel data from
+            linePointer[0] = r * linePointer[3] / 255;
+            linePointer[1] = g * linePointer[3] / 255;
+            linePointer[2] = b * linePointer[3] / 255;
+            linePointer += 4;
+        }
+    }
+    
+    // get a CG image from the context, wrap that into a
+    // UIImage
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    UIImage *returnImage = [UIImage imageWithCGImage:cgImage];
+    
+    // clean up
+    CGImageRelease(cgImage);
+    CGContextRelease(context);
+    free(memoryPool);
+    
+    // and return
+    return returnImage;
+}
+
+- (UIImage *)convertImageToGrayScale:(UIImage *)image
+{
+    // Create image rectangle with current image width/height
+    CGRect imageRect = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    // Grayscale color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    
+    // Create bitmap content with current image size and grayscale colorspace
+    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone);
+    
+    // Draw image into current context, with specified rectangle
+    // using previously defined context (with grayscale colorspace)
+    CGContextDrawImage(context, imageRect, [image CGImage]);
+    
+    // Create bitmap image info from pixel data in current context
+    CGImageRef imageRef = CGBitmapContextCreateImage(context);
+    
+    // Create a new UIImage object
+    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+    
+    // Release colorspace, context and bitmap information
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    CFRelease(imageRef);
+    
+    // Return the new grayscale image
+    return newImage;
+}
+    
+- (UIImage *)increaseBrightness:(double)brightness andContrast:(double)contrast for:(UIImage *)image
+{
+    CIImage* ciImage = [CIImage imageWithCGImage:image.CGImage];
+    
+    ciImage = [CIFilter filterWithName:@"CIHueAdjust" keysAndValues:kCIInputImageKey, ciImage,@"inputAngle", @0.8, nil].outputImage;
+    
+    UIImage* tri =  [[UIImage alloc] initWithCIImage:ciImage];
+    
+    return tri;
+}
+    
 @end
-  
